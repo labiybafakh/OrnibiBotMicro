5403e27395ce106b21c57c3a07788d2849fe4e47
labiybafakh <labiybafakh@gmail.com>
2023-01-15 20:53:19 +0900

    update wing position

--- include/OrnibibBot.hpp
+++ include/OrnibibBot.hpp
@@ -13,14 +13,31 @@
 // #include "OrnibiBot.hpp"
 
 
+#define sine 1
+#define square 2
+#define saw 3
+#define rev_saw 4
+#define triangle 5
+
 class OrnibiBot{
         
     private:
+        double scalar = 1;
+        
         volatile uint16_t getFlapMs();
         struct tailPosition{
             unsigned int roll;
             unsigned int pitch;
         };
+
+        volatile int16_t sineFlap();
+        volatile int16_t squareFlap();
+        volatile int16_t sawFlap();
+        volatile int16_t triangleFlap();
+        volatile int16_t reverse_sawFlap();
+        uint32_t getRawPosition(uint8_t pin);
+        double getPositioninRadians(uint8_t pin);
+        double getPositioninDegrees(uint8_t pin);
 
     public:
         volatile int16_t _offset;
@@ -30,11 +47,7 @@ class OrnibiBot{
         volatile uint16_t _periode;
         volatile uint16_t _flapping;
 
-        volatile int16_t sineFlap();
-        volatile int16_t squareFlap();
-        volatile int16_t sawFlap();
-        volatile int16_t triangleFlap();
-        volatile int16_t reverse_sawFlap();
+        volatile uint16_t flappingPattern(uint8_t pattern);
 
         tailPosition tail_position;
 
